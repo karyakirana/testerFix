69b4c20eb22d4cf36b956769ffffe7b8a526baf5
kirana <adibpratama2@gmail.com>
2021-08-07 09:41:48 +0700

    update

--- routes/stock.php
+++ routes/stock.php
@@ -2,5 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Stock Keluar
 Route::get('/stock/keluar', [\App\Http\Controllers\Stock\StockKeluarController::class, 'index']);
-Route::get('/stock/new', [\App\Http\Controllers\Stock\StockKeluarController::class, 'create'])->name('stockKeluarNew');
+Route::get('/stock/keluar/new', [\App\Http\Controllers\Stock\StockKeluarController::class, 'create'])->name('stockKeluarNew');
+
+// Stock Temp Trans
+Route::post('/stock/temp', [\App\Http\Controllers\Stock\StockTempController::class, 'store']);
+Route::get('/stock/temp/{id}', [\App\Http\Controllers\Stock\StockTempController::class, 'edit']);
+Route::delete('/stock/temp/{id}', [\App\Http\Controllers\Stock\StockTempController::class, 'destroy']);
