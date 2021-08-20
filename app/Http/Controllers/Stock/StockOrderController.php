ca8791e5a866ee2c7767ff7a2a7d1ad1be93c926
kirana <adibpratama2@gmail.com>
2021-08-21 00:08:55 +0700

    update predator

--- app/Http/Controllers/Stock/StockOrderController.php
+++ app/Http/Controllers/Stock/StockOrderController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Stock;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sales\PenjualanTemp;
 use App\Models\Stock\StockOrder;
+use App\Models\Stock\StockTemp;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class StockOrderController extends Controller
 {
@@ -13,9 +18,46 @@ class StockOrderController extends Controller
         return view('pages.stock.stockOrder');
     }
 
+    public function createTemp($idStockOrder)
+    {
+        $create = StockTemp::create([
+            'jenisTemp' => 'StockOrder',
+            'idSales' => Auth::id(),
+            'stockMasuk'=> $idStockOrder,
+        ]);
+
+        return $create;
+    }
+
+    private function checkLastCart()
+    {
+        // check session stock
+        if (session('stockMasuk'))
+        {
+            // jika ada langsung ambil data stock
+            $stock = StockTemp::find(session('stockOrder'));
+        } else {
+            // check last temp
+            $lastTemp = StockTemp::where('idUser', Auth::id())->where('jenisTemp', 'StockOrder')->whereNull('stockMasuk');
+            if ($lastTemp->count() > 0)
+            {
+                // jika ada
+                $stock = $lastTemp->latest()->first();
+            } else {
+                $stock = $this->createTemp();
+            }
+            session()->put(['stockOrder'=>$stock->id]);
+        }
+        $data = [
+            'idTemp'=>$stock->id,
+            'idUser'=>$stock->idUser
+        ];
+        return $data;
+    }
+
     public function create()
     {
-        return view('pages.stock.stockOrderTrans');
+        return view('pages.stock.stockOrderTrans', $this->checkLastCart());
     }
 
     public function show($id)
@@ -34,9 +76,34 @@ class StockOrderController extends Controller
         return view('pages.stock.stockOrderTrans');
     }
 
+    public function kodeStockOrder()
+    {
+        $data = StockOrder::where('activeCash', session('ClosedCash'))->latest('kode')->first();
+        $num = null;
+        if(!$data){
+            $num = 1;
+        } else {
+            $urutan = (int) substr($data->kode, 0, 4);
+            $num = $urutan + 1;
+        }
+        $id = sprintf("%04s", $num)."/SO/".date('Y');
+        return $id;
+    }
+
     public function store(Request $request)
     {
-        //
+        $idTemp = $request->idTemp;
+        $kode = $this->kode();
+        $tglOrder = date('Y-m-d', strtotime($request->tglOrder));
+
+        DB::beginTransaction();
+        try {
+            // insert stock Order
+
+            DB::commit();
+        } catch (ModelNotFoundException $e){
+            DB::rollBack();
+        }
     }
 
     public function update(Request $request)
--- app/Http/Datatables/StockOrderTable.php
+++ app/Http/Datatables/StockOrderTable.php
@@ -3,14 +3,42 @@
 namespace App\Http\Datatables;
 
 use App\Models\Stock\StockOrder;
+use App\Models\Stock\StockOrderDetil;
 use Yajra\DataTables\DataTables;
 
 class StockOrderTable {
 
     public function stockOrderList()
     {
-        $data = StockOrder::where('activeCash', session('ClosedCash'))->get();
-        return DataTables::of($data);
+        $data = StockOrder::with(['suppliers'])->where('activeCash', session('ClosedCash'))->get();
+        return DataTables::of($data)
+            ->addColumn('suppliers', function($row){
+                return $row->suppliers->namaSupplier ?? '';
+            })
+            ->addColumn('Action', function($row){
+                $edit = '<a href="#" class="btn btn-sm btn-clean btn-icon" id="btnEdit" data-value="'.str_replace('/', '-', $row->id_jual).'" title="edit"><i class="la la-edit"></i></a>';
+                $show = '<a href="#" class="btn btn-sm btn-clean btn-icon" id="btnShow" data-value="'.str_replace('/', '-', $row->id_jual).'" title="show"><i class="flaticon2-indent-dots"></i></a>';
+                $delete = '<a href="#" class="btn btn-sm btn-clean btn-icon" id="btndelete" data-value="'.$row->id_jual.'" title="delete"><i class="flaticon2-trash"></i></a>';
+                $print = '<a href="#" class="btn btn-sm btn-clean btn-icon" id="btnPrint" data-value="'.str_replace('/', '-', $row->id_jual).'" title="print"><i class="flaticon-technology"></i></a>';
+                return $edit.$show.$delete.$print;
+            })
+            ->rawColumns(['Action'])
+            ->make(true);
+    }
+
+    public function stockOrderDetilList($idStockOrder)
+    {
+        $data = StockOrderDetil::with('produk')->latest()->get();
+        return DataTables::of($data)
+            ->addIndexColumn()
+            ->addColumn('produk', function ($row){
+                $produk = $row->produk->nama_produk ?? '';
+                $cover = $row->produk->cover ?? '';
+                $kat_harga = $row->produk->kategoriHarga->nama_kat ?? '';
+                return $produk.'<br>'.$cover.'-'.$kat_harga;
+            })
+            ->rawColumns(['produk'])
+            ->make(true);
     }
 
 }
