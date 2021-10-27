211e1b0c5806441d3498b20282013802bd16ed33
karyakirana <adibpratama2@gmail.com>
2021-10-27 18:53:50 +0700

    dari macbook air #refactoring

--- resources/views/pages/accounting/kasirPemasukanTrans.blade.php
+++ resources/views/pages/accounting/kasirPemasukanTrans.blade.php
@@ -1,5 +1,40 @@
 <x-MetronicsLayout>
 
-
+    <div class="card card-custom gutter-b">
+        <div class="card-header">
+            <div class="card-title"></div>
+            <div class="card-toolbar">
+                <input type="text" class="form-control">
+            </div>
+        </div>
+        <form>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-8">
+                        <table class="table table-bordered"></table>
+                    </div>
+                    <div class="col-md-4 border">
+                        <h4 class="mt-5">Form</h4>
+                        <div class="form-group row mb-6">
+                            <label class="col-lg-3 col-form-label" for="">ID</label>
+                            <div class="col-lg-9">
+                                <input type="text" name="idPengeluaran" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group row mb-6">
+                            <label class="col-lg-3 col-form-label" for="">ID</label>
+                            <div class="col-lg-9">
+                                <input type="text" name="idPengeluaran" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <div class="card-footer d-flex justify-content-between">
+            <a href="#" class="btn btn-light-primary font-weight-bold">Manage</a>
+            <a href="#" class="btn btn-outline-secondary font-weight-bold">Learn more</a>
+        </div>
+    </div>
 
 </x-MetronicsLayout>
